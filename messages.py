3514b80bb5cbf2f13e87ffe3d79e2ad10ac95e20
Alex Takács <alextakacs10@gmail.com>
2017-03-29 14:53:51 +0200

    Added welcome message, control setter and control printer functions.

---
+++ messages.py
@@ -0,0 +1,24 @@
+def name_prompt():
+    username = input("Your name: ")
+    return username
+
+
+def welcome_message(username):
+    print((len("Your name: " + username)) * "-")
+    print("\nWelcome, {}!\n".format(username))
+
+
+def set_controls():
+    print("Please set the controls of movement:")
+    up = input("Move up: ")
+    down = input("Move down: ")
+    left = input("Move left: ")
+    right = input("Move right: ")
+    return up, down, left, right
+
+
+def defined_controls(controls):
+    print("\nControls:")
+    print("Up: [{}] | Down: [{}] | Left: [{}] | Right: [{}]".format(controls[0]
+          , controls[1], controls[2], controls[3]))
+    print("Press [q] to exit.\n")
